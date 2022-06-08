883cd5afd93d6a52d5f2ba3713193ab2148cfb03
iFanpS <89406191+iFanpSGTS@users.noreply.github.com>
2022-06-08 12:17:19 +0700

    V-0.2 Alpha

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from colorama import Fore; import dir, cmds; import random;
+from colorama import *; import dir, cmds; import random
 #IFP = iFanpS Prompt
 
 ################## Variable ################################
+init()
 dirs = dir.dir()
 CMDs = cmds.CMD()
 rand_ver = random.randint(1000, 2000)
@@ -9,22 +10,61 @@ Information_ifNotRunAsAdmin = Fore.GREEN + """
 IFP Windows (not run as admin) [11.0.{0}.{1}]
 (c) IFP CMD by iFanpS
 """.format(rand_ver, rand_ver).strip()
+Information_ifRunasAdmin = Fore.LIGHTGREEN_EX + """
+IFP Windows (run as admin) [11.0.{0}.{1}]
+(c) IFP CMD by iFanpS
+""".format(rand_ver, rand_ver).strip()
 SyntaxOrSymbol = "[IFP] <{0}> />"
+CMDs.change_title()
 ############################################################
 
-def Selection_Cmd():
-    print(Information_ifNotRunAsAdmin)
-    while True:
-        cmd = input("\n"+SyntaxOrSymbol.format(dirs.curr_dir()))
-        if cmd.startswith("cd"):
-            dirs.change_dir(cmd.split()[1])
-        if cmd.startswith("listdir"):
-            dirs.listDir(dirs.curr_dir())
-        if cmd.startswith("mkdir"):
-            dirs.makeDir(cmd.split()[1], dirs.curr_dir())
-        if cmd.startswith("mkfile"):
-            dirs.createFile(cmd.split()[1], dirs.curr_dir())
-        if cmd.startswith("date"):
-            CMDs.date()
+if CMDs.isAdmin():
+    def Selection_Cmd():
+        print(Information_ifRunasAdmin)
+        while True:
+            cmd = input("\n"+SyntaxOrSymbol.format(dirs.curr_dir()))
+            if cmd.startswith("cd"):
+                dirs.change_dir(cmd)
+            if cmd.startswith("listdir"):
+                dirs.listDir(dirs.curr_dir())
+            if cmd.startswith("mkdir"):
+                dirs.makeDir(cmd.split()[1], dirs.curr_dir())
+            if cmd.startswith("mkfile"):
+                dirs.createFile(cmd.split()[1], dirs.curr_dir())
+            if cmd.startswith("date"):
+                CMDs.date()
+            if cmd.startswith("clear"):
+                CMDs.clear_prompt()
+            if cmd.startswith("help"):
+                CMDs.help_cmd()
+            if cmd.startswith("echo"):
+                CMDs.print_text(cmd.replace("echo", ""))
+            if cmd.startswith("ver"):
+                CMDs.platform("ver")
+else:
+    def Selection_Cmd():
+        print(Information_ifNotRunAsAdmin)
+        while True:
+            cmd = input("\n"+SyntaxOrSymbol.format(dirs.curr_dir()))
+            if cmd.startswith("cd"):
+                dirs.change_dir(cmd)
+            if cmd.startswith("listdir"):
+                CMDs.change_title("listdir")
+                dirs.listDir(dirs.curr_dir())
+            if cmd.startswith("mkdir"):
+                dirs.makeDir(cmd.split()[1], dirs.curr_dir())
+            if cmd.startswith("mkfile"):
+                dirs.createFile(cmd.split()[1], dirs.curr_dir())
+            if cmd.startswith("date"):
+                CMDs.date()
+            if cmd.startswith("clear"):
+                CMDs.change_title("clear")
+                CMDs.clear_prompt()
+            if cmd.startswith("help"):
+                CMDs.help_cmd()
+            if cmd.startswith("echo"):
+                CMDs.print_text(cmd.replace("echo", ""))
+            if cmd.startswith("ver"):
+                CMDs.platform("ver")
         
 Selection_Cmd()
