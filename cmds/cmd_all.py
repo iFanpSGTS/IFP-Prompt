7b3f994218d0c6e1a682c29edb1c8f2af45667be
iFanpS <89406191+iFanpSGTS@users.noreply.github.com>
2022-06-10 17:48:12 +0700

    V-1.1 Beta

--- cmds/cmd_all.py
+++ cmds/cmd_all.py
@@ -1,16 +1,62 @@
-from datetime import date; import os, ctypes, platform
+from datetime import date; import os, ctypes, platform,time
 
-commds = ["mkdir", "listdir", "mkfile", "date", "clear", "echo", "ver (display windows version)"]
+import psutil;from error import *;from art import *;from colorama import *
+
+help_commds = ["mkdir", "listdir", "mkfile", "date", "clear", "echo", "ver (display windows version)", "cd", "start", "crp (check running program)"]
+avai_commds = ["mkdir", "listdir", "mkfile", "date", "clear", "echo", "ver", "cd", "help", "start", "crp"]
+init()
 
 class CMD:
     def __init__(self):
         pass
     
-    def platform(self, arg):
-        if arg == "ver":
-            print(f"[Windows] {platform.version()}")
-        else:
+    def check_available_cmd(self, cmd):
+        try:
+            if cmd.split()[0] not in avai_commds:
+                defind_error("CommandNotFoundError")
+            else:
+                pass
+        except:
             pass
+    
+    def Interface(self, isAdmin=False):
+        if isAdmin == False:
+            print(f"""{Fore.CYAN + text2art("IF Prompt")}
+    {Fore.GREEN}
+    \tIFP Windows (not run as admin) [{platform.version()}]
+    \t(c) IFP CMD by iFanpS <2022>          
+    """)
+        else:
+            print(f"""{Fore.CYAN + text2art("IF Prompt")}
+    \tIFP Windows (run as admin) [{platform.version()}]
+    \t(c) IFP CMD by iFanpS <2022>          
+    """)
+    
+    def platform(self, arg):
+        osname = os.name
+        crp_count_program = 0
+        if arg == "ver":
+            if osname == "nt":
+                osname = "Windows"
+                print(f"<{osname}> {platform.version()}")
+            else:
+                print(f"<{osname}> {platform.version()}")
+        if arg == "crp":
+            print(" \n [/] Checking all running program")
+            for i in psutil.win_service_iter():
+                crp_count_program += 1
+                print(f"[> {i} </]")
+                time.sleep(0.1)
+            if crp_count_program >= 700:
+                print(Fore.RED + f"[warning] {crp_count_program} program is running, its to high." + Fore.GREEN)
+            else:
+                print(f"[i] {crp_count_program} program is running.")
+    
+    def start_program(self, name_or_path):
+        try:
+            return os.startfile(f"{name_or_path.split()[1]}")
+        except:
+            print("[i] Cannot run the specified program.")
     
     def date(self):
         print(f"\nDate : [ Make sure u happy today :D ] <{date.today()}>")
@@ -36,7 +82,7 @@ class CMD:
             pass 
         else:
             print(" CMD Available:\n")
-            for i in commds:
+            for i in help_commds:
                 print("[<>] " + i)
                 
     def print_text(self, text):
