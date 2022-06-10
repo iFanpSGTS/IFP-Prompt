764c25fd4197491e1f5097da8f7801705e39968b
iFanpS <89406191+iFanpSGTS@users.noreply.github.com>
2022-06-10 17:47:39 +0700

    V-0.1 Alpha

---
+++ error/err.py
@@ -0,0 +1,19 @@
+import os
+errs = {
+    "FileNotFoundError":"[i] Cannot find the specified path/file or folder.",
+    "FileExistsError":"[i] File/Folder already exist.",
+    "AttributeError":"[x] Attribute error.",
+    "IndexError":"[i] Cannot process syntax from input.",
+    "KeyError":"[x] Key Error.",
+    "ModuleNotFoundError":"[i] Module is not available on internal or external.",
+    "SyntaxError":"[x] Syntax error.",
+    "BaseException":"[x] BaseException error.",
+    "AdminError":"[i] Please run as admin.",
+    "CDError":"[dir] <{0}>".format(os.getcwd()),
+    "CommandNotFoundError":"[x] CMD is not available."
+}
+def defind_error(error):
+    if error in errs.keys():
+        print(errs.get(error))
+    else:
+        print("[x] Cant proceed the error, from custom errorHandler.")
